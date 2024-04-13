53bf038fe4db320260dd94791ba5af7cd3c8f8fe
rico <845380186@qq.com>
2024-04-13 15:36:15 +0800

    引入element-plus及图标、引入normalize.scss初始化scss

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { setupElementPlus } from '@/plugins/elementPlus'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/normalize.scss'
+
 const app = createApp(App)
 
+// 引入elementPlus相关
+setupElementPlus(app)
 app.use(createPinia())
 app.use(router)
 
