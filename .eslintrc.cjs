02bb16695b41b755737c72c395ac3b8b2acca748
rico <845380186@qq.com>
2024-04-16 23:44:18 +0800

    处理eslint和prettier兼容的问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 // eslint:recommended：该配置集是ESlint内置的“推荐”，它打开一组小的、合理的规则，用于检查众所周知的最佳实践
 // @typescript-eslint/recommended：该配置集是typescript-eslint的推荐，它与eslint:recommended相似，但它启用了特定于ts的规则
 // @typescript-eslint/eslint-recommended：该配置集禁用eslint:recommended配置集中已经由typescript处理的规则，防止eslint和typescript之间的冲突
+// prettier（即eslint-config-prettier）关闭所有可能干扰Prettier规则的Eslint规则，确保将其放在最后，这样它有机会覆盖其他配置
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
@@ -12,13 +13,20 @@ module.exports = {
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended'
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier'
   ],
   rule: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 'error', // 打开prettier插件提供的规则，该插件从 ESLint 内运行 Prettier
+    // 关闭这两个 ESLint 核心规则，这两个规则和prettier插件一起使用会出现问题，具体可参阅
+    // https://github.com/prettier/eslint-plugin-prettier/blob/master/README.md#arrow-body-style-and-prefer-arrow-callback-issue
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
   },
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  plugins: ['@typescript-eslint']
-}
+  // prettier插件（即eslint-plugin-prettier）将prettier规则转换为Eslint规则
+  plugins: ['@typescript-eslint', 'prettier']
+};
