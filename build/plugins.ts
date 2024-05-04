cc295dd6b7e9c5cba7d331c35ab680ab6cbd0808
rico <845380186@qq.com>
2024-05-04 16:36:30 +0800

    feat: 完善vite插件

--- build/plugins.ts
+++ build/plugins.ts
@@ -3,13 +3,17 @@ import { PluginOption } from 'vite';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import eslintPlugin from 'vite-plugin-eslint';
+import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 /**
  * 创建 vite 插件
  * @param viteEnv
  */
 export const createVitePlugins = (viteEnv: ViteEnv): (PluginOption | PluginOption[])[] => {
-  console.log(viteEnv);
+  const { VITE_GLOB_APP_TITLE, VITE_REPORT } = viteEnv;
   return [
     vue(),
     // vue 可以使用 jsx/tsx 语法
@@ -18,6 +22,49 @@ export const createVitePlugins = (viteEnv: ViteEnv): (PluginOption | PluginOptio
     createSvgIconsPlugin({
       iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[dir]-[name]'
-    })
+    }),
+    // 注入变量到 html 文件
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: { title: VITE_GLOB_APP_TITLE }
+      }
+    }),
+    // esLint 报错信息显示在浏览器界面上
+    eslintPlugin(),
+    // 创建打包压缩配置
+    createCompression(viteEnv),
+    // 是否生成包预览，分析依赖包大小做优化处理
+    VITE_REPORT &&
+      (visualizer({ open: true, filename: 'stats.html', gzipSize: true, brotliSize: true }) as PluginOption)
   ];
+};
+
+/**
+ * @description 根据 compress 配置，生成不同的压缩规则
+ * @param viteEnv
+ */
+const createCompression = (viteEnv: ViteEnv): PluginOption | PluginOption[] => {
+  const { VITE_BUILD_COMPRESS = 'none', VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
+  const compressList = VITE_BUILD_COMPRESS.split(',');
+  const plugins: PluginOption[] = [];
+  if (compressList.includes('gzip')) {
+    plugins.push(
+      viteCompression({
+        ext: '.gz',
+        algorithm: 'gzip',
+        deleteOriginFile: VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE
+      })
+    );
+  }
+  if (compressList.includes('brotli')) {
+    plugins.push(
+      viteCompression({
+        ext: '.br',
+        algorithm: 'brotliCompress',
+        deleteOriginFile: VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE
+      })
+    );
+  }
+  return plugins;
 };
